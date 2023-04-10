8f61c5dabca39f821850af3e1c3c7acbab182dd2
Marianne Pissaia <marianne.p03@aluno.ifsc.edu.br>
2023-04-10 20:35:12 -0300

    Criação dos arquivos Servidor.js e Rotas.js

---
+++ servidor.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const routes = require("./rotas")
+
+const server = express()
+
+server.use(routes)
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+
+
+server.listen(3000,()=>{
+    console.log("Sevidor em execução...")
+})
+
